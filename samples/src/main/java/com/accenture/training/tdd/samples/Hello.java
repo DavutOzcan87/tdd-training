6be604b79569bc1aa935e2111c88934418d32c92
davut.ozcan <davut.ozcan@accenture.com>
2019-02-25 13:10:48 +0300

    canary test working

---
+++ samples/src/main/java/com/accenture/training/tdd/samples/Hello.java
@@ -0,0 +1,4 @@
+package com.accenture.training.tdd.samples;
+
+public class Hello {
+}
---
+++ samples/src/test/java/com/accenture/training/tdd/samples/HelloTest.java
@@ -0,0 +1,14 @@
+package com.accenture.training.tdd.samples;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTest {
+
+
+    @Test
+    public void shouldPass(){
+        assertTrue(true);
+    }
+}
