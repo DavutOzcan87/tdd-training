86c8f379016108efcb894797aea803676bd27a5c
davut.ozcan <davut.ozcan@accenture.com>
2019-02-26 15:17:37 +0300

    exchange calculator first test runs

---
+++ exchange-calculator/src/test/java/com/accenture/training/tdd/exchangecalculator/ExchangeServiceTest.java
@@ -0,0 +1,18 @@
+package com.accenture.training.tdd.exchangecalculator;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class ExchangeServiceTest {
+
+
+
+    @Test
+    public void sampleTest() {
+        assertThat(true).isTrue();
+    }
+
+}
